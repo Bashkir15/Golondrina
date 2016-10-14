689449ba7bf317ce6812279c2fe8a0b7556c6c3e
Forrest Collins <Forest.d.collins@gmail.com>
2016-10-14 18:11:25 -0500

    Added the layout for all view port sizes on portfolio main page

--- server/routes/index.server.routes.js
+++ server/routes/index.server.routes.js
@@ -5,4 +5,12 @@ router.get('/', (req, res) => {
 	res.render('index');
 });
 
+router.get('/portfolio', (req, res) => {
+	res.render('./views/pages/portfolio/portfolio');
+});
+
+router.get('/portfolio/canvas', (req, res) => {
+	res.render('./views/pages/portfolio/canvas');
+});
+
 module.exports = router;
