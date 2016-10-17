0a0f0c83717250c9021d1a4556cf5985d56a4d87
Forrest Collins <Forest.d.collins@gmail.com>
2016-10-17 14:08:22 -0500

    unfortunately, time constraints have made it where I will no longer be able to utilize the isomorphic idea that I wanted. Went back to original version

--- server/config/express.js
+++ server/config/express.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import path from 'path';
 import compression from 'compression';
-import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import ejs from 'ejs';
 
 import indexRoutes from '../routes/index.server.routes';
+
 
 module.exports = (db) => {
 	const app = express();
@@ -13,8 +13,7 @@ module.exports = (db) => {
 	app.set('view engine', 'ejs');
 	app.set('views', path.join(__dirname, '../../public'));
 
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({extended: true}));
+
 	app.use(morgan('dev'));
 	app.use(compression());
 	app.use((req, res, next) => {
