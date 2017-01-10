73f7aca9a47956789504dbef5a17d59a210011c3
Forrest Collins <Forest.d.collins@gmail.com>
2017-01-10 13:26:30 -0600

    Added some simple lazy loading to test, will write a plugin now

--- dist/bundle.js
+++ dist/bundle.js
@@ -3083,6 +3083,48 @@
 				return element.getElementsByTagName('img')[0].alt;
 			}
 		});
+
+		function loadImage(element, fn) {
+			var img = new Image();
+			var src = element.getAttribute('data-src');
+
+			img.onload = function () {
+				if (!!element.parent) {
+					element.parent.replaceChild(img, element);
+				} else {
+					element.src = src;
+				}
+
+				fn ? fn() : null;
+			};
+
+			img.src = src;
+		}
+
+		function isInViewport(element) {
+			var rect = element.getBoundingClientRect();
+
+			return rect.top >= 0 && rect.left >= 0 && rect.top <= (window.innerHeight || document.documentElement.clientHeight);
+		}
+
+		var images = new Array();
+		var query = document.querySelectorAll('.lazy');
+		var progressScroll = function progressScroll() {
+			for (var i = 0; i < images.length; i++) {
+				if (isInViewport(images[i])) {
+					loadImage(images[i], function () {
+						images.splice(i, i);
+					});
+				}
+			}
+		};
+
+		for (var i = 0; i < query.length; i++) {
+			images.push(query[i]);
+		}
+
+		progressScroll();
+		window.addEventListener('scroll', progressScroll);
 	}
 
 /***/ },
